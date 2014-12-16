d9ea4caac86c274af11f953b33c5cabe18337a83
Rob Dimsdale <robert.dimsdale@gmail.com>
2014-12-15 22:05:39 -0800

    Add HTTPTransportHelper tests.

---
+++ http_transport_helper_test.go
@@ -0,0 +1,34 @@
+package wundergo_test
+
+import (
+	"net/http"
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/robdimsdale/wundergo"
+)
+
+var _ = Describe("Default HTTP Transport Helper", func() {
+	var transportHelper *wundergo.DefaultHTTPTransportHelper
+
+	BeforeEach(func() {
+		transportHelper = wundergo.NewDefaultHTTPTransportHelper()
+	})
+
+	Describe("NewRequest", func() {
+		It("Returns a new http.Request without error", func() {
+			method := "aMethod"
+			urlStr := "aUrl"
+			body := strings.NewReader("someBody")
+
+			expectedRequest, err := http.NewRequest(method, urlStr, body)
+			Expect(err).NotTo(HaveOccurred())
+
+			request, err := transportHelper.NewRequest(method, urlStr, body)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(request).To(Equal(expectedRequest))
+		})
+	})
+})
