38f47a1d8e5c58c3f770b7c512647ca5a4b32c4e
Rob Dimsdale <robert.dimsdale@gmail.com>
2015-01-18 14:31:21 -0800

    Fix list integration test.
    - Wunderlist lists now +1 to revision upon create.

--- integration_tests/wundergo_list_integration_test.go
+++ integration_tests/wundergo_list_integration_test.go
@@ -1,6 +1,8 @@
 package wundergo_integration_test
 
 import (
+	"fmt"
+
 	"github.com/nu7hatch/gouuid"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -11,11 +13,11 @@ var _ = Describe("Basic list functionality", func() {
 	It("performs CRUD for lists", func() {
 		uuid1, err := uuid.NewV4()
 		Expect(err).NotTo(HaveOccurred())
-		newListTitle1 := uuid1.String()
+		newListTitle1 := fmt.Sprintf("%s-original", uuid1.String())
 
 		uuid2, err := uuid.NewV4()
 		Expect(err).NotTo(HaveOccurred())
-		newListTitle2 := uuid2.String()
+		newListTitle2 := fmt.Sprintf("%s-updated", uuid2.String())
 
 		var originalLists *[]wundergo.List
 		Eventually(func() error {
@@ -29,13 +31,7 @@ var _ = Describe("Basic list functionality", func() {
 			return err
 		}).Should(Succeed())
 
-		var newLists *[]wundergo.List
-		Eventually(func() error {
-			newLists, err = client.Lists()
-			return err
-		}).Should(Succeed())
-		Expect(listContains(newLists, newList)).To(BeTrue())
-
+		newList.Revision = newList.Revision + 1
 		newList.Title = newListTitle2
 		var updatedList *wundergo.List
 		Eventually(func() error {
@@ -44,6 +40,13 @@ var _ = Describe("Basic list functionality", func() {
 		}).Should(Succeed())
 		newList.Revision = newList.Revision + 1
 		Expect(updatedList).To(Equal(newList))
+
+		var newLists *[]wundergo.List
+		Eventually(func() error {
+			newLists, err = client.Lists()
+			return err
+		}).Should(Succeed())
+		Expect(listContains(newLists, newList)).To(BeTrue())
 
 		Eventually(func() error {
 			newList, err = client.List(newList.ID)
