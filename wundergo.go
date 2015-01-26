f98a8dbc577739e997e8149fdf93391ae4fcc0cc
Rob Dimsdale <robert.dimsdale@gmail.com>
2015-01-25 19:14:11 -0800

    Split OauthClient into its own file.
    [#85185660]

---
+++ wundergo.go
@@ -0,0 +1,10 @@
+/*
+Package wundergo provides a client to the Wunderlist API.
+
+For more information on the API, see https://developer.wunderlist.com/documentation
+*/
+package wundergo
+
+const (
+	apiURL = "https://a.wunderlist.com/api/v1"
+)
