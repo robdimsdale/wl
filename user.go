e22059c1083e5c3ab344624ace044a2a071cc0b7
Rob Dimsdale <robert.dimsdale@gmail.com>
2015-02-14 12:06:13 -0800

    Remove user.TypeString
    [#86857828]

--- user.go
+++ user.go
@@ -4,11 +4,10 @@ import "time"
 
 // User contains information about a User.
 type User struct {
-	ID         uint      `json:"id"`
-	Name       string    `json:"name"`
-	Email      string    `json:"email"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
-	Revision   uint      `json:"revision"`
-	TypeString string    `json:"type"`
+	ID        uint      `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Revision  uint      `json:"revision"`
 }
