5d1671bb9bf53a61f5c8db8a00f8c62adcf33e81
Rob Dimsdale <robert.dimsdale@gmail.com>
2015-10-14 18:42:58 -0700

    Reduce flakiness in root test.
    [#105727604]

--- integration_tests/root_test.go
+++ integration_tests/root_test.go
@@ -3,12 +3,17 @@ package wl_integration_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/robdimsdale/wl"
 )
 
 var _ = Describe("basic root functionality", func() {
 	It("gets root correctly", func() {
-		root, err := client.Root()
-		Expect(err).NotTo(HaveOccurred())
+		var err error
+		var root wl.Root
+		Eventually(func() error {
+			root, err = client.Root()
+			return err
+		}).Should(Succeed())
 
 		Expect(root.ID).To(BeNumerically(">", 0))
 	})
